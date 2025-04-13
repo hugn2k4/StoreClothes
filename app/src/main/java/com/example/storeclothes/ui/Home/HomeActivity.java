ebd2a49e9a5448c38f9bbf4efd8e318635a437b2
Hung Le <hcl2k4@gmail.com>
2025-04-14 04:10:42 +0700

    format folder

--- app/src/main/java/com/example/storeclothes/ui/Home/HomeActivity.java
+++ app/src/main/java/com/example/storeclothes/ui/Home/HomeActivity.java
@@ -1,4 +1,4 @@
-package com.example.storeclothes.ui;
+package com.example.storeclothes.ui.Home;
 
 import android.os.Bundle;
 
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.storeclothes.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_home);
     }
 }
