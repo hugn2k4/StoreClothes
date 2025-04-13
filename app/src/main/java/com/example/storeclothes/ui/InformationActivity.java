eadcb3369dd1255d095ab48ecf8579acab836470
Hung Le <hcl2k4@gmail.com>
2025-04-14 04:07:20 +0700

    ui base

---
+++ app/src/main/java/com/example/storeclothes/ui/InformationActivity.java
@@ -0,0 +1,20 @@
+package com.example.storeclothes.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.storeclothes.R;
+
+public class InformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
