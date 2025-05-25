c3ba8c64fd551f75be5620bd3b60e83f92977d7f
NhatMinhTran-2611 <137895310+NhatMinhTran-2611@users.noreply.github.com>
2025-05-26 00:20:46 +0700

    delete Stratery

--- app/src/main/java/com/example/storeclothes/ui/Strategy/EntityActionStrategy.java
+++
@@ -1,8 +0,0 @@
-package com.example.storeclothes.ui.Strategy;
-
-public interface EntityActionStrategy {
-    void add(Entity entity);
-    void edit(Entity entity);
-    void delete(Entity entity);
-    void toggleStatus(Entity entity);
-}
